fdc0a99a46fd53673eeb7d81e90dd2ab2762a8b9
TimeWX <18827388851@163.com>
2019-04-10 17:02:33 +0800

    Initial commit

---
+++ utils/config.js
@@ -0,0 +1,18 @@
+
+// 数据请求地址
+var host = "127.0.0.1:8081/mini"
+
+var config = {
+
+  // 下面的地址配合云端 Server 工作
+  host: `http://${host}/`, 
+
+  // 数据请求接口地址
+  requestUrl: `http://${host}/Huod/`,
+
+  // 百度AK填写，用于获取地理位置
+  baiduAk: '123123123123123'
+  
+};
+
+module.exports = config
